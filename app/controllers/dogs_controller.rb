5133f3365eaacfb85ba9064dbaf41ceb487ba71f
Flatiron School <andrewsprague@outlook.com>
2018-09-26 11:20:16 -0500

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,28 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    
+  end
+
+  def edit
+  end
+
+  private
+
+  def dogs_params
+    params.require().permit()
+  end
+
+  
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,12 @@
 class Employee < ApplicationRecord
+
+    belongs_to :dog
+
+    validates :title, uniqueness: true
+    validates :alias, uniqueness: true
+    
+
+    def name
+        self.first_name + " " + self.last_name
+    end
 end
