4c199dc0e84bd302f1de5692e40bd47dc88a7b98
Kim Pham <pham.kim.a@gmail.com>
2018-09-26 11:19:22 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    # No one at Dunder Mifflin can have the same alias and/or job title (Dwight made up that rule!)
+    # validates :alias, uniqueness: true
+    # validates :title, uniqueness: true
 end
