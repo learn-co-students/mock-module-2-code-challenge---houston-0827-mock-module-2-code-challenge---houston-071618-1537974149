7593cfe28be17aa1bfee4fd5ab3c2443f784b759
gwatson86 <gwatson86@gmail.com>
2018-09-26 11:20:15 -0500

    WHY WILL THE EMPLOYEES NOT SEED

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :alias, uniqueness: true
+    validates :title, presence: true
+    validates :title, uniqueness: true
+    validates :office, presence: true
+    validates :dog_id, presence: true
 end
