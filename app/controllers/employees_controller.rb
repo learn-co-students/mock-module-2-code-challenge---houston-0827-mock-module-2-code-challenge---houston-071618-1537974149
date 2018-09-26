96b79311c2948f0b10a10aa958c0189ce4765035
V10LET <violet.wmoon@gmail.com>
2018-09-26 11:21:58 -0500

    Dunder Mifflin Dogshow App

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,34 @@
 class EmployeesController < ApplicationController
+  def index
+    @es = Employee.all
+  end
+
+  def show
+    @e = Employee.find(params[:id])
+  end
+
+  def new
+    @e = Employee.new
+  end
+
+  def create
+    @e = Employee.create(e_params)
+    redirect_to employee_path(@e)
+  end
+
+  def edit
+    @e = Employee.find(params[:id])
+  end
+
+  def update
+    @e = Employee.find(params[:id])
+    @e.update(e_params)
+    redirect_to employee_path(@e)
+  end
+
+private
+  def e_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
+
 end
